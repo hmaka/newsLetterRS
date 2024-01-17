3b5ef1780c54bc6f5183956757eae96a1f4a67f7
Havish <Havish747@gmail.com>
2024-01-16 20:09:46 -0500

    Made changes to database configuration script

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,14 @@ pub struct Settings {
     pub database: DataBaseSettings,
     pub application_port: u16,
 }
-
+impl DataBaseSettings {
+    pub fn connection_string(&self) -> String {
+        return format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        );
+    }
+}
 impl TryFrom<config::Config> for Settings {
     type Error = config::ConfigError;
     fn try_from(value: config::Config) -> Result<Self, Self::Error> {
@@ -14,16 +21,6 @@ impl TryFrom<config::Config> for Settings {
     }
 }
 
-//impl From<config::Config> for Settings {
-//    fn from(config: config::Config) -> Self {
-//        // You will need to extract the relevant fields from the config object and use them to construct a Settings object.
-//        // This is just a placeholder. Replace it with your actual logic.
-//        Settings {
-//            database: config.get("database"),
-//            application_port: config.get("application_port"),
-//        }
-//    }
-//}
 #[derive(serde::Deserialize)]
 pub struct DataBaseSettings {
     pub username: String,
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use newsletter_rs::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
+
 fn spawn_app() -> String {
     let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listner.local_addr().unwrap().port();
@@ -27,8 +30,14 @@ async fn health_check_works() {
 #[actix_web::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
-    let client = reqwest::Client::new();
+    let configuration = get_configuration().expect("Failed to rad configuration");
+    let connection_string = configuration.database.connection_string();
 
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let client = reqwest::Client::new();
     let mock_body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let response = client
@@ -40,10 +49,17 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_web::test]
-
 async fn subscribe_returns_a_400_when_data_is_missing() {
     let address = spawn_app();
     let client = reqwest::Client::new();
